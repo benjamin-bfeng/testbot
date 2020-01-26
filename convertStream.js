c43149d1eb9207f6a5e021819809df9b34d12207
Glenn Sun <sun.glenn@outlook.com>
2020-01-26 01:06:46 -0800

    basic transcription

---
+++ convertStream.js
@@ -0,0 +1,24 @@
+const { Transform } = require('stream')
+
+function convertBufferTo1Channel(buffer) {
+  const convertedBuffer = Buffer.alloc(buffer.length / 2)
+
+  for (let i = 0; i < convertedBuffer.length / 2; i++) {
+    const uint16 = buffer.readUInt16LE(i * 4)
+    convertedBuffer.writeUInt16LE(uint16, i * 2)
+  }
+
+  return convertedBuffer
+}
+
+class ConvertTo1ChannelStream extends Transform {
+  constructor(source, options) {
+    super(options)
+  }
+
+  _transform(data, encoding, next) {
+    next(null, convertBufferTo1Channel(data))
+  }
+}
+
+exports.ConvertTo1ChannelStream = ConvertTo1ChannelStream
--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const Discord = require('discord.js')
 const config = require('./config')
+const {ConvertTo1ChannelStream} = require('./convertStream')
+const {playFile} = require('./media')
+const googleSpeech = require('@google-cloud/speech')
+
+const googleSpeechClient = new googleSpeech.SpeechClient()
 
 const discordClient = new Discord.Client()
 
@@ -8,3 +13,56 @@ discordClient.on('ready', () => {
 })
 
 discordClient.login(config.discordApiToken)
+
+discordClient.on('message', async (message) => {
+  if (message.content == "!join") {
+    const member = message.member
+    const memberVoiceChannel = member.voice.channel
+
+    const connection = await memberVoiceChannel.join()
+    const receiver = connection.receiver
+
+    await playFile(connection, 'wrongChannelEn.mp3')
+
+    connection.on('speaking', (user, speaking) => {
+      if (!speaking) {
+        return
+      }
+
+      console.log(`I'm listening to ${user.username}`)
+
+      // this creates a 16-bit signed PCM, stereo 48KHz stream
+      const audioStream = receiver.createStream(user, { mode: 'pcm' })
+      const requestConfig = {
+        encoding: 'LINEAR16',
+        sampleRateHertz: 48000,
+        languageCode: 'en-US'
+      }
+      const request = {
+        config: requestConfig
+      }
+      const recognizeStream = googleSpeechClient
+        .streamingRecognize(request)
+        .on('error', console.error)
+        .on('data', response => {
+          const transcription = response.results
+            .map(result => result.alternatives[0].transcript)
+            .join('\n')
+            .toLowerCase()
+          console.log(`Transcription: ${transcription}`)
+        })
+
+      const convertTo1ChannelStream = new ConvertTo1ChannelStream()
+
+      audioStream.pipe(convertTo1ChannelStream).pipe(recognizeStream)
+
+      audioStream.on('end', async () => {
+        console.log('audioStream end')
+      })
+    })
+  } else if (message.content == "!leave") {
+    const member = message.member
+    const memberVoiceChannel = member.voice.channel
+    memberVoiceChannel.leave()
+  }
+})
